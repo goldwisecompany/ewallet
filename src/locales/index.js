b8a1859013bdb6ab0727ccf551802ac3c8c7b4a0
goldwisecompany <goldwisecompany@gmail.com>
2020-07-14 03:07:27 +0800

    update: i18n module and dependency

---
+++ src/locales/index.js
@@ -0,0 +1,11 @@
+import LocalizedStrings from 'react-native-localization';
+
+import en from './en.json';
+import zh from './zh_tw.json';
+
+let strings = new LocalizedStrings({
+  en,
+  zh,
+});
+
+export default strings;
